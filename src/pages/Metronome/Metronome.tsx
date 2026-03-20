adfe7c2496e24d3e3303978bae6db315d28a5b9f
Sara Romao <sara-romao@live.com>
2026-03-20 12:27:22 -0300

    added title

--- src/pages/Metronome/Metronome.tsx
+++ src/pages/Metronome/Metronome.tsx
@@ -3,6 +3,7 @@ import { Metronome } from "../../components/Metronome"
 export default function MetronomePage() {
   return (
     <div>
+      <h1 className="text-2xl font-bold text-center">Metrônomo</h1>
       <Metronome/>
     </div>
   );
