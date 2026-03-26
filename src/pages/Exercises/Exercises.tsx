4ec967b8b5bfda6a69b4581b9d048b7807e46f65
Sara Romao <sara-romao@live.com>
2026-03-25 23:26:19 -0300

    modified layout and added dynamic

--- src/pages/Exercises/Exercises.tsx
+++ src/pages/Exercises/Exercises.tsx
@@ -1,6 +1,40 @@
+import { useParams } from "react-router-dom";
+import { Metronome } from "../../components/Metronome";
+import { progressionList } from "../../data/progression";
 
-export function Exercises(){
-  return(
-    <h1>Exer</h1>
-  )
+type Params = {
+  id?: string;
+};
+
+export function Exercises() {
+  const { id } = useParams<Params>();
+  const chord = progressionList.find((item) => item.id === id);
+  const prog: string[] = chord?.chords ?? [];
+
+  return (
+    <>
+      {!chord ? (
+        <header>
+          <h2 className="text-xl text-center font-bold text-gray-900 sm:text-3xl">
+            Exercício Não encontrado
+          </h2>
+        </header>
+      ) : (
+        <>
+          <header>
+            <h2 className="text-xl text-center font-bold text-gray-900 sm:text-3xl">
+              {chord?.key}
+              {chord?.type === "major" ? " Maior" : " Menor"}
+            </h2>
+          </header>
+
+          <p className="mt-4 text-center text-lg mx-auto max-w-md text-gray-500">
+            Progressão {chord.chords.join(" - ")}
+          </p>
+
+          <Metronome prog={prog} />
+        </>
+      )}
+    </>
+  );
 }
