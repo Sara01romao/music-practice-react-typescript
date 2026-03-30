a100933b37d9a89220f221c6852a4fb83880aae6
Sara Romao <sara-romao@live.com>
2026-03-30 20:21:11 -0300

    added not found and link in logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Exercises } from "./pages/Exercises/Exercises";
 import "./App.css";
 import { Menu } from "./components/Menu";
 import Chords from "./pages/Chords/Chords";
 import MetronomePage from "./pages/Metronome/Metronome";
-import logo from "./assets/ms-logo.svg"
+import logo from "./assets/ms-logo.svg";
 
 export function App() {
   return (
     <BrowserRouter>
       <div className="w-full">
         <div className="flex justify-center w-full p-4">
-          <img src={logo} alt="" className="flex mx-auto max-w-[100px]" />
+          <Link to="/">
+            <img src={logo} alt="Logo" className="flex mx-auto max-w-[100px]" />
+          </Link>
         </div>
         <Menu />
         <Routes>
@@ -20,6 +22,7 @@ export function App() {
           <Route path="/metronome" element={<MetronomePage />} />
           <Route path="/chords/:type" element={<Chords />} />
           <Route path="/exercise/:type/:id" element={<Exercises />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
       </div>
     </BrowserRouter>
