e44573dfb842a6c18463c02b6e7d76b628d2ff3a
Sara Romao <sara-romao@live.com>
2026-03-30 10:21:40 -0300

    added barre to some chords

--- src/data/chords.ts
+++ src/data/chords.ts
@@ -1,22 +1,65 @@
+export type Barre = {
+  fret: number;
+  fromString: number;
+  toString: number;
+};
+
 export type ChordType = {
   frets: number[];
   type: string;
+   barre?: Barre;
 };
 
 export const chordList: Record<string, ChordType> = {
-  A: { frets: [-1, 0, 2, 2, 2, 0], type: "Maior" },
-  B: { frets: [-1, 2, 4, 4, 4, 2], type: "Maior" },
-  C: { frets: [-1, 3, 2, 0, 1, 0], type: "Maior" },
-  D: { frets: [-1, -1, 0, 2, 3, 2], type: "Maior" },
-  E: { frets: [0, 2, 2, 1, 0, 0], type: "Maior" },
-  F: { frets: [1, 3, 3, 2, 1, 1], type: "Maior" },
-  G: { frets: [3, 2, 0, 0, 0, 3], type: "Maior" },
+  C: { frets: [-1, 3, 2, 0, 1, 0], type: "major" },
 
-  Am: { frets: [-1, 0, 2, 2, 1, 0], type: "Menor" },
-  Bm: { frets: [-1, 2, 4, 4, 3, 2], type: "Menor" },
-  Cm: { frets: [-1, 3, 5, 5, 4, 3], type: "Menor" },
-  Dm: { frets: [-1, -1, 0, 2, 3, 1], type: "Menor" },
-  Em: { frets: [0, 2, 2, 0, 0, 0], type: "Menor" },
-  Fm: { frets: [1, 3, 3, 1, 1, 1], type: "Menor" },
-  Gm: { frets: [3, 5, 5, 3, 3, 3], type: "Menor" },
+  D: { frets: [-1, -1, 0, 2, 3, 2], type: "major" },
+
+  E: { frets: [0, 2, 2, 1, 0, 0], type: "major" },
+
+  F: {
+    frets: [1, 3, 3, 2, 1, 1],
+    type: "major",
+    barre: { fret: 1, fromString: 6, toString: 1 },
+  },
+
+  G: { frets: [3, 2, 0, 0, 0, 3], type: "major" },
+
+  A: { frets: [-1, 0, 2, 2, 2, 0], type: "major" },
+
+  B: {
+    frets: [-1, 2, 4, 4, 4, 2],
+    type: "major",
+    barre: { fret: 2, fromString: 6, toString: 2 },
+  },
+
+  Cm: {
+    frets: [-1, 3, 5, 5, 4, 3],
+    type: "minor",
+    barre: { fret: 3, fromString: 6, toString: 1 },
+  },
+
+  Dm: { frets: [-1, -1, 0, 2, 3, 1], type: "minor" },
+
+  Em: { frets: [0, 2, 2, 0, 0, 0], type: "minor" },
+
+  Fm: {
+    frets: [1, 3, 3, 1, 1, 1],
+    type: "minor",
+    barre: { fret: 1, fromString: 6, toString: 1 },
+  },
+
+  Gm: {
+    frets: [3, 5, 5, 3, 3, 3],
+    type: "minor",
+    barre: { fret: 3, fromString: 6, toString: 1 },
+  },
+
+  Am: { frets: [-1, 0, 2, 2, 1, 0], type: "minor" },
+
+  Bm: {
+    frets: [-1, 2, 4, 4, 3, 2],
+    type: "minor",
+    barre: { fret: 2, fromString: 6, toString: 1 },
+  },
 };
