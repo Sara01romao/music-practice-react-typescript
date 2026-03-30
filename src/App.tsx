1d924b0992c5037cd668604e9f2ca4b7bad8d6c1
Sara Romao <sara-romao@live.com>
2026-03-30 10:18:53 -0300

    config routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,24 @@ import { Exercises } from "./pages/Exercises/Exercises";
 import "./App.css";
 import { Menu } from "./components/Menu";
 import Chords from "./pages/Chords/Chords";
-import Metronome from "./pages/Metronome/Metronome";
+import MetronomePage from "./pages/Metronome/Metronome";
+import logo from "./assets/ms-logo.svg"
 
 export function App() {
   return (
     <BrowserRouter>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/metronome" element={<Metronome />} />
-        <Route path="/chords/:type" element={<Chords />} />
-        <Route path="/exercise/:type/:id" element={<Exercises />}/>
-      </Routes>
+      <div className="w-full">
+        <div className="flex justify-center w-full p-4">
+          <img src={logo} alt="" className="flex mx-auto max-w-[100px]" />
+        </div>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/metronome" element={<MetronomePage />} />
+          <Route path="/chords/:type" element={<Chords />} />
+          <Route path="/exercise/:type/:id" element={<Exercises />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
